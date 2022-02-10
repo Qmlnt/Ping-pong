b9ad89dceb21bb47dea65c2cb08367e4045ab1ce
BuildTools <unconfigured@null.spigotmc.org>
2022-02-10 17:22:14 +0200

    Player class
    Added Player class and two Players

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ WINDOW_CAPTION = "Ping pong"
 WINDOW_ICON_IMG  = "icon.png"
 BACKGROUND_IMG = "background.jpg"
 BALL_IMG = "ball.png"
-BALL_DIMENSIONS = (50,50)
-ball_x, ball_y = 300, 200
+BALL_DIMENSIONS = (50,50)       # Width, height
+PLAYER_DIMENSIONS = (30, 150)
+PLAYER_SPEED = 8
+PLAYER1_UP = pg.K_w
+PLAYER1_DOWN = pg.K_s
+PLAYER2_UP = pg.K_UP
+PLAYER2_DOWN = pg.K_DOWN
+PLAYER_COLOR = (0,255,255)        # RGB
+PLAYER1_X = 10
+PLAYER_Y = int((WINDOW_HEIGHT-PLAYER_DIMENSIONS[1])/2)
+PLAYER2_X = WINDOW_WIDTH-PLAYER_DIMENSIONS[0]-PLAYER1_X
+BALL_X = int((WINDOW_WIDTH-BALL_DIMENSIONS[0])/2)
+BALL_Y = int((WINDOW_HEIGHT-BALL_DIMENSIONS[0])/2)
 
 pg.display.set_caption(WINDOW_CAPTION)
 pg.display.set_icon(pg.image.load(WINDOW_ICON_IMG))
@@ -19,7 +30,7 @@ timer = pg.time.Clock()
 
 
 class GameSprite(pg.sprite.Sprite):
-    def __init__(self, image:pg.Surface, x:int, y:int, speed:float=0) -> None:
+    def __init__(self, image: pg.Surface, x: int, y: int, speed: float = 0) -> None:
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
@@ -30,8 +41,24 @@ class GameSprite(pg.sprite.Sprite):
     def draw(self):
         main_window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def __init__(self, image: pg.Surface, x: int, y: int, speed: float, up: int, down: int, color: tuple) -> None:
+        super().__init__(image, x, y, speed)
+        self.image.fill(color)
+        self.up = up
+        self.down = down
+    
+    def update(self) -> None:
+        keys = pg.key.get_pressed()
+        if keys[self.up] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        elif keys[self.down] and self.rect.y < WINDOW_HEIGHT-PLAYER_DIMENSIONS[1]:
+            self.rect.y += self.speed
 
-ball = GameSprite(pg.transform.scale(pg.image.load(BALL_IMG), BALL_DIMENSIONS), ball_x, ball_y)
+
+ball = GameSprite(pg.transform.scale(pg.image.load(BALL_IMG), BALL_DIMENSIONS), BALL_X, BALL_Y)
+player1 = Player(pg.Surface(PLAYER_DIMENSIONS), PLAYER1_X, PLAYER_Y, PLAYER_SPEED, PLAYER1_UP, PLAYER1_DOWN, PLAYER_COLOR)
+player2 = Player(pg.Surface(PLAYER_DIMENSIONS), PLAYER2_X, PLAYER_Y, PLAYER_SPEED, PLAYER2_UP, PLAYER2_DOWN, PLAYER_COLOR)
 
 executing = True
 while executing:
@@ -42,7 +69,11 @@ while executing:
 
     main_window.blit(background, (0,0))
 
+    player1.update()
+    player2.update()
     ball.draw()
+    player1.draw()
+    player2.draw()
 
     pg.display.update()
     timer.tick(FPS)
