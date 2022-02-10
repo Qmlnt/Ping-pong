74799709ccd525a864459cc3b2add7056ec2fe52
BuildTools <unconfigured@null.spigotmc.org>
2022-02-10 16:28:02 +0200

    GameSprite class
    Added a class named GameSprite and a ball to test it

--- main.py
+++ main.py
@@ -3,16 +3,35 @@ import pygame as pg
 FPS = 60
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 500
-WINDOW_ICON  = "icon.png"
 WINDOW_CAPTION = "Ping pong"
+WINDOW_ICON_IMG  = "icon.png"
 BACKGROUND_IMG = "background.jpg"
+BALL_IMG = "ball.png"
+BALL_DIMENSIONS = (50,50)
+ball_x, ball_y = 300, 200
 
 pg.display.set_caption(WINDOW_CAPTION)
-pg.display.set_icon(pg.image.load(WINDOW_ICON))
+pg.display.set_icon(pg.image.load(WINDOW_ICON_IMG))
 main_window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 background = pg.transform.scale(pg.image.load(BACKGROUND_IMG), (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 timer = pg.time.Clock()
+
+
+class GameSprite(pg.sprite.Sprite):
+    def __init__(self, image:pg.Surface, x:int, y:int, speed:float=0) -> None:
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+    
+    def draw(self):
+        main_window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+ball = GameSprite(pg.transform.scale(pg.image.load(BALL_IMG), BALL_DIMENSIONS), ball_x, ball_y)
 
 executing = True
 while executing:
@@ -23,7 +42,7 @@ while executing:
 
     main_window.blit(background, (0,0))
 
-
+    ball.draw()
 
     pg.display.update()
     timer.tick(FPS)
